3c605a5c16ec262adfb85f4edb40a3f273914217
Abrar Yeasir <162937581+iamabraryeasir@users.noreply.github.com>
2025-11-02 15:50:46 +0600

    Initial project setup completed.

---
+++ lib/auth.ts
@@ -0,0 +1,14 @@
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+import config from "@/config";
+
+const client = new MongoClient(config.mongodb.uri);
+const db = client.db();
+
+export const auth = betterAuth({
+    database: mongodbAdapter(db, {
+        client,
+    }),
+});
